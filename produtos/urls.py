e03c9ba1ce8152dbe154ea58f30056aa1842fa32
Sagrob <bieldiniz27@gmail.com>
2024-07-01 21:33:07 -0300

    embalagens

--- produtos/urls.py
+++ produtos/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
 
-from .views import adicionar_local, inicio, listar_locais
+from .views import (
+    adicionar_embalagem,
+    adicionar_local,
+    inicio,
+    listar_embalagens,
+    listar_locais,
+)
 
 urlpatterns = [
     path('', inicio, name='inicio'),
     path('locais/', listar_locais, name='listar_locais'),
     path('locais/adicionar', adicionar_local, name='adicionar_local'),
+    path('embalagens/', listar_embalagens, name='listar_embalagens'),
+    path('embalagens/adicionar', adicionar_embalagem, name='adicionar_embalagem'),  # noqa: E501
+
 ]
